7953108acf578d5eb97674d0d01529e050b55ab8
re.013080000 <104734867+re-013080000@users.noreply.github.com>
2026-03-13 19:29:11 +0100

    chunk finished

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -10,19 +10,26 @@ pub struct Chunk {
 
 impl Chunk {
     pub fn new(chunk_pos: IVec3, fill: BlockType) -> Result<Self, OctreeError> {
-        todo!()
+        match Octree::new(CHUNK_SIZE, fill) {
+            Err(error)  => Err(error),
+            Ok(tree)    => Ok(Self { chunk_pos, octree: tree, dirty: false }),
+        }
     }
 
     pub fn get_local(&self, pos: IVec3) -> Result<BlockType, OctreeError> {
-        todo!()
+        self.octree.get(pos)
     }
 
     pub fn set_local(&mut self, pos: IVec3, block: BlockType) -> Result<(), OctreeError> {
-        todo!()
+        self.octree.set(pos, block)?;
+        self.dirty = true;
+        Ok(())
     }
 
     pub fn remove_local(&mut self, pos: IVec3) -> Result<(), OctreeError> {
-        todo!()
+        self.octree.remove(pos)?;
+        self.dirty = true;
+        Ok(())
     }
 
     pub fn is_dirty(&self) -> bool {
