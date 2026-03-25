ea7800af70fc160f0638ac28ec08aeeb0b3d76a4
GoDoc <roni-sinan.yildiz@epita.fr>
2026-03-25 20:59:06 +0100

    merge de procGen avec main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod world;
 mod rendering;
 mod proc_gen;
 fn main() {
-    proc_gen::bruit_main::noise_main();
+    //proc_gen::bruit_main::noise_main();
+    //test génération procédurale.
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(rendering::camera::CameraPlugin)
